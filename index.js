08bdf0a6a3ea0c6f896727454c3e74c17d62227c
kaique64 <kaiquehenrique.2005@outlook.com>
2024-10-08 22:50:18 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import path from 'path'
+
+const app = express();
+const PORT = 5000
+
+// Get dirname to send absolute file path to render
+const dirname = process.cwd();
+
+app.get('/',function(_, res) {
+    res.sendFile(path.join(`${dirname}/index.html`));
+});
+
+app.get('/style.css', function(_, res) {
+    res.sendFile(dirname + "/style.css");
+});
+
+app.get('/images/img.png', function(_, res) {
+    res.sendFile(dirname + "/images/img.png");
+});
+
+app.get('/images/logo.png', function(_, res) {
+    res.sendFile(dirname + "/images/logo.png");
+});
+
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`))

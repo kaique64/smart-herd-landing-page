94c40542eee6aa140066da956b4a3f20d30b2620
Kaique Costa <kaiquec@ciandt.com>
2024-10-15 00:01:00 -0300

    feat: add logging

--- server/api_client/upload.py
+++ server/api_client/upload.py
@@ -1,19 +1,19 @@
+from flask import current_app
 import requests
 from constants.constants import API_KEY, ROBOFLOW_URL
 
-
 def upload_image_predict(base64_image):
     try:
-        print('Calling model api')
+        current_app.logger.info(f'Calling model API: {ROBOFLOW_URL}')
         params = {'api_key': API_KEY}
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
 
         response = requests.post(ROBOFLOW_URL, data=base64_image, params=params, headers=headers)
         response.raise_for_status()
 
-        print('Model api called successfully')
+        current_app.logger.info('Model API called successfully')
         return response.json()
 
     except requests.RequestException as error:
-        print(f'Error in upload: {str(error)}')
+        current_app.logger.error(f'Error in upload: {str(error)}')
         return None
--- server/app.py
+++ server/app.py
@@ -3,26 +3,33 @@ from flask_cors import CORS
 from flask import request
 from dotenv import load_dotenv
 from constants.constants import FLASK_RUN_HOST
-
 from api_client.upload import upload_image_predict
+import logging
 
 load_dotenv()
 
 app = Flask(__name__)
 cors = CORS(app)
 
-@app.route('/model-img-predict', methods = ['POST'])
+# Set up logging
+logging.basicConfig(level=logging.DEBUG)  # Capture all levels: DEBUG, INFO, WARNING, ERROR
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+@app.route('/model-img-predict', methods=['POST'])
 def model_image_predict():
     try:
         app.logger.info('Init calling model')
         result = upload_image_predict(request.data)
+        
         if result is None:
+            app.logger.warning('Prediction result is None')
             raise Exception("Prediction result is None")
+        
+        app.logger.info('Prediction successful')
         return jsonify(result), 200
     except Exception as e:
         app.logger.error(f"An error occurred: {str(e)}")
         return jsonify({"error": "An error occurred during prediction"}), 500
 
-
 if __name__ == '__main__':
-    app.run(port=3000, host=FLASK_RUN_HOST, debug=True)
+    app.run(port=3000, host=FLASK_RUN_HOST, debug=False)
